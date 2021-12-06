f09536042ec0c9015e27aca0f423a3783b718bdc
sabrilassouli <sabri.lassouli@student.ehb.be>
2021-12-06 19:53:59 +0100

    moving files

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const express = require('express')();
 const app = express;
 const port = process.env.PORT || 3000;
 
+
 //middleware
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
